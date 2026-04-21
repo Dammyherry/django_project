cb7defa1634f42f5fcc93c724de122be650307a6
Damola <doluniran1@ivytech.edu>
2026-04-21 21:27:54 +0100

    Fix homepage route properly

--- mysite/urls.py
+++ mysite/urls.py
@@ -3,5 +3,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('myapp.urls')),   # 👈 THIS LINE IS IMPORTANT
+    path('', include('myapp.urls')),  # MUST be here
 ]
